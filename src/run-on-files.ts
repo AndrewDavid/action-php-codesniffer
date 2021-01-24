303b67ff97b382d8446883552cfc07c659578ab4
Andrew-David J <hello@andrewdavid.net>
2021-01-24 15:34:53 -0500

    Update run-on-files.ts

--- src/run-on-files.ts
+++ src/run-on-files.ts
@@ -7,7 +7,11 @@ import * as core from '@actions/core';
  */
 export function runOnCompleteFiles(files: string[]): number {
   const phpcs = core.getInput('phpcs_path', { required: true });
+  
   const args = ['--report=checkstyle'];
+  
+  const report = core.getInput('report');
+  if (report) args.push(`--report=json ${report}`);
 
   const standard = core.getInput('standard');
   if (standard) args.push(`--standard=${standard}`);
